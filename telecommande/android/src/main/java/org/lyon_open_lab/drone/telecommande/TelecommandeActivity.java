02640ff0427c97aee7d63a7f4db3e4c22a1c6fdc
Charles de MAGNEVAL <magneval@gmail.com>
2013-11-19 00:09:09 +0100

    Première version de l'application

--- telecommande/android/src/main/java/org/lyon_open_lab/drone/telecommande/TelecommandeActivity.java
+++ telecommande/android/src/main/java/org/lyon_open_lab/drone/telecommande/TelecommandeActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.webkit.WebView;
 
-public class HelloAndroidActivity extends Activity {
-
+public class TelecommandeActivity extends Activity {
+    WebView mWebView;
+  
     /**
      * Called when the activity is first created.
      * @param savedInstanceState If the activity is being re-initialized after 
@@ -17,14 +19,19 @@ public class HelloAndroidActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWebView= (WebView)findViewById(R.id.webview);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("http://lyonopenlab-org.github.io/Drone/telecommande/touchScreen.html");
     }
 
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 	// Inflate the menu; this adds items to the action bar if it is present.
 	getMenuInflater().inflate(org.lyon_open_lab.drone.telecommande.R.menu.main, menu);
 	return true;
     }
+    */
 
 }
 
